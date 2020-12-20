144e2c1e6ae0c29d63924399ecbaef3c32384cbe
RebelDroid09 <paulcs91@gmail.com>
2020-12-19 18:21:11 -0600

    Dropwdown Setup and Dropdown Event Setup
    Loaded data into the dropdown box, and working on pulling data based on dropdown selection event.

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+const dataset;
+
+d3.json("./data/samples.json").then(function(data)
+{ 
+    dataset = data;
+
+    console.log(data);
+
+    subjectNames = dataset.names;
+
+    var dropdown = d3.select("#selDataset");
+
+    var subjectOptions = dropdown.selectAll('option')
+        .data(subjectNames)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        }); 
+});
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function updatePlotly() {
+
+    var dropdownMenu = d3.select("#selDataset");
+
+    var desiredDataset = dropdownMenu.property("value");
+
+    var desiredIndex = dataset.names.indexOf(desiredDataset);
+
+    var sampleSet = dataset.samples[desiredIndex];
+
+    var otuIdSet = sampleSet.otu_ids.slice(0, 11);
+    var otuLabelSet = sampleSet.otu_labels.slice(0, 11);
+    var sampleValueSet = sampleSet.sample_values.slice(0, 11);
+
+};
+
